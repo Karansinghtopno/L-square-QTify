aec28cf065b9fd0d612f8764992466b4e84036fb
karan <karansinghtopno@gmail.com>
2023-03-05 15:57:50 +0530

    C2:Button

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Logo = () => {
-  return <img src={require("../../assets/logo.png")} alt="logo"  width={67}/>;
+  return (
+    <img
+      src={require("../../assets/logo.png")}
+      alt="logo"
+      width={67}
+      style={{ cursor: "pointer" }}
+    />
+  );
 };
 
 export default Logo;
--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '../Button/Button'
 import Logo from '../Logo/Logo'
 import styles from "./Navbar.module.css"
 
@@ -8,7 +9,14 @@ import styles from "./Navbar.module.css"
 const Navbar = () => {
   return (
     <div className={styles.navbar} >
+
+
+        <div className={styles.wrapper}> 
         <Logo/>
+        <Button text="Give Feedback" />
+        </div>
+       
+        
     </div>
   )
 }
