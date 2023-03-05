4ebe91e292a808dc294df382a8b73acca8f0170d
karan <karansinghtopno@gmail.com>
2023-03-05 15:43:15 +0530

    C2:navbar

---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from '../Logo/Logo'
+import styles from "./Navbar.module.css"
+
+
+
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar} >
+        <Logo/>
+    </div>
+  )
+}
+
+export default Navbar
